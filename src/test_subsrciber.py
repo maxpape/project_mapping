339cc054d2590429dbc187b1d0308e207930aca4
Max Pape <maxpape@mail.uni-paderborn.de>
2023-09-19 09:38:11 +0200

    add ros

---
+++ src/test_subsrciber.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import rospy
+import open3d as o3d
+import numpy as np
+from ctypes import * # convert float to uint32
+from std_msgs.msg import Header
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+
+
+def convertCloudFromRosToOpen3d(ros_cloud):
+    
+    # Get cloud data from ros_cloud
+    field_names=[field.name for field in ros_cloud.fields]
+    cloud_data = list(pc2.read_points(ros_cloud, skip_nans=True, field_names = field_names))
+
+    # Check empty
+    open3d_cloud = o3d.geometry.PointCloud()
+    if len(cloud_data)==0:
+        print("Converting an empty cloud")
+        return None
+        
+    # Set open3d_cloud
+    if "rgb" in field_names:
+        cloud_data = [cloud[0:4] for cloud in cloud_data]
+        IDX_RGB_IN_FIELD=3 # x, y, z, rgb
+        
+        # Get xyz
+        xyz = [(x,y,z) for x,y,z,rgb in cloud_data ] # (why cannot put this line below rgb?)
+
+        # Get rgb
+        # Check whether int or float
+        if type(cloud_data[0][IDX_RGB_IN_FIELD])==float: # if float (from pcl::toROSMsg)
+            rgb = [convert_rgbFloat_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
+        else:
+            rgb = [convert_rgbUint32_to_tuple(rgb) for x,y,z,rgb in cloud_data ]
+
+        # combine
+        open3d_cloud.points = o3d.utility.Vector3dVector(np.array(xyz))
+        open3d_cloud.colors = o3d.utility.Vector3dVector(np.array(rgb)/255.0)
+    else:
+        cloud_data = [cloud[0:3] for cloud in cloud_data]
+        xyz = [(x,y,z) for x,y,z in cloud_data ] # get xyz
+        open3d_cloud.points = o3d.utility.Vector3dVector(np.array(xyz))
+
+    # return
+    return open3d_cloud
+
+def callback(data):
+    # This function will be called when a new message is received
+    pcd = convertCloudFromRosToOpen3d(data)
+    o3d.visualization.draw_geometries([pcd])
+    
+    rospy.loginfo("Received a PointCloud2 message")
+
+def main():
+    rospy.init_node('pointcloud_subscriber', anonymous=True)
+    rospy.Subscriber('/periodic_snapshotter/assembled_cloud_2', PointCloud2, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+ 
